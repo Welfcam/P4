9e0a45ea87f3e6d2686dff244ed9073a361549be
Welfcam <welfcam@gmail.com>
2024-10-30 10:13:10 +0100

    LWC and labels

---
+++ salesforce_projet_4/force-app/main/default/lwc/opportunityProductViewer/labels.js
@@ -0,0 +1,28 @@
+//Liste des Custom labels utilisés
+import No_Opp_Prod from '@salesforce/label/c.No_Opp_Prod';
+import Delete from '@salesforce/label/c.Delete';
+import Opp_Update_Qty_Alert from '@salesforce/label/c.Opp_Update_Qty_Alert';
+import Opportunity_Products from '@salesforce/label/c.Opportunity_Products';
+import Product_Name from '@salesforce/label/c.Product_Name';
+import Quantity from '@salesforce/label/c.Quantity';
+import Quantity_alert from '@salesforce/label/c.Quantity_alert';
+import Quantity_in_stock from '@salesforce/label/c.Quantity_in_stock';
+import See_Product from '@salesforce/label/c.See_Product';
+import Total_Price from '@salesforce/label/c.Total_Price';
+import Unit_Price from '@salesforce/label/c.Unit_Price';
+import Opp_Prod_List_Error from '@salesforce/label/c.Opp_Prod_List_Error';
+
+export default {
+    No_Opp_Prod,
+    Delete,
+    Opp_Update_Qty_Alert,
+    Opportunity_Products,
+    Product_Name,
+    Quantity,
+    Quantity_alert,
+    Quantity_in_stock,
+    See_Product,
+    Total_Price,
+    Unit_Price,
+    Opp_Prod_List_Error
+};
